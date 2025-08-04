728274afba512d66bb9e6c8c2615706dfefc34d1
Md. Mehedi Hasan <mehedi.cseulab201@gmail.com>
2025-08-04 16:43:55 +0600

    add binary search tree implementation

---
+++ data-structure/binary-search-tree/binarySearchTree.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Value int
+	Left *Node 
+	Right *Node
+}
+
+func (n *Node) Insert(value int) *Node {
+	if n == nil {
+		return &Node{Value: value}
+	}
+
+	if value < n.Value {
+		n.Left = n.Left.Insert(value)
+	} else if value > n.Value {
+		n.Right = n.Right.Insert(value)
+	}
+
+	return n
+}
+
+func (n *Node) Search(value int) bool {
+	if n == nil {
+		return false
+	}
+
+	if value == n.Value {
+		return true
+	} else if (value < n.Value) {
+		return n.Left.Search(value)
+	} else {
+		return n.Right.Search(value)
+	}
+}
+
+func (n *Node) InOrder() {
+	if n == nil {
+		return
+	}
+
+	n.Left.InOrder()
+	fmt.Print(n.Value, " ")
+	n.Right.InOrder()
+}
+
+func (n *Node) PreOrder() {
+	if n == nil {
+		return
+	}
+
+	fmt.Print(n.Value, " ")
+	n.Left.PreOrder()
+	n.Right.PreOrder()
+}
+
+func (n *Node) PostOrder() {
+	if n == nil {
+		return
+	}
+
+	n.Left.PostOrder()
+	n.Right.PostOrder()
+	fmt.Print(n.Value, " ")
+}
+
+func main() {
+	var root *Node
+
+	values := []int{3, 1, 2, 5, 4, 6}
+	for _, v := range values {
+		root = root.Insert(v)
+	}
+
+	// root.InOrder()
+	// root.PreOrder()
+	root.PostOrder()
+	fmt.Println()
+}
